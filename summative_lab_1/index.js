cfca24382d5fd56a0cc4ceeef688a7a5a577885a
Nick Rathbone <nrathbone@joinhandshake.com>
2025-06-07 19:40:12 -0600

    Create the history functions to log each calculation and display the history array after each calculation

--- summative_lab_1/index.js
+++ summative_lab_1/index.js
@@ -34,4 +34,22 @@ function divide(a, b) {
 
 // Create History Functions
 
+function logCalculation(a, b, operator, result) {
+    history.push({ // Push an object with keys
+        operand1: a,
+        operand2: b,
+        operator: operator,
+        result: result
+    });
+}
 
+function displayHistory() {
+    if (history.length === 0) { // Check to see if the `history` array is empty
+        console.log("No calculations yet.");
+    } else {
+        for (let i = 0; i < history.length; i++) {
+            const entry = history[i]; // Get the current object from the `history` array at position `i`
+            console.log(`${entry.operand1} ${entry.operator} ${entry.operand2} = ${entry.result}`); // Prints a formatted string using values from the current entry
+        }
+    }
+}
