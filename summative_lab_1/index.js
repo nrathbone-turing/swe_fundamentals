b5589d2cbeaeca909bd0fc40e4694c0912006701
Nick Rathbone <nrathbone@joinhandshake.com>
2025-06-07 19:26:25 -0600

    Create the operation functions for add, subtract, multiply, and divide

--- summative_lab_1/index.js
+++ summative_lab_1/index.js
@@ -4,7 +4,33 @@ let history = [] // Creates an empty array
 
 // Create Operation Functions
 
+function add(a, b) {
+    const result = a + b;
+    logCalculation(a, b, '+', result);
+    return result;
+}
 
+function subtract(a, b) {
+    const result = a - b;
+    logCalculation(a, b, '-', result);
+    return result;
+}
+
+function multiply(a, b) {
+    const result = a * b;
+    logCalculation(a, b, '*', result);
+    return result;
+}
+
+function divide(a, b) {
+    if (b === 0) {
+        return "Error: Cannot divide by zero."
+    } else {
+        const result = a / b;
+        logCalculation(a, b, '/', result);
+        return result;
+    }
+}
 
 // Create History Functions
 
